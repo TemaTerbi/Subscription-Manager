dbd3867f24659c41ac7fcad6ae8bd439aaeb1489
TemaTerbi <tematerbi@mail.ru>
2022-12-27 11:18:26 +0300

    Initial Commit

---
+++ SubscriptionManager/SubscriptionManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SubscriptionManagerApp.swift
+//  SubscriptionManager
+//
+//  Created by Артем Соловьев on 27.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SubscriptionManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
